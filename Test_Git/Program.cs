132abd29f50030e6dfd0707b44cddb4e6c860b49
padmasuja <sgpadmasuja@gmail.com>
2021-08-10 11:02:30 +0530

    changed message

--- Test_Git/Program.cs
+++ Test_Git/Program.cs
@@ -6,7 +6,7 @@ namespace Test_Git
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World sita!");
         }
     }
 }
